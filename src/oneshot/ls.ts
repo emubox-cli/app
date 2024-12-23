82173cd7d478e917f2d76ee5fb78f13d4d7362fe
skullbite <skullbite@users.noreply.github.com>
2024-12-23 14:32:58 +0000

    Upload build files to new branch `8a0f9fc`

--- src/oneshot/ls.ts
+++
@@ -1,5 +0,0 @@
-import apps from "../utils/apps";
-
-export default function() {
-    console.log(apps.filter(d => d.installOptions.aur).map(d => `- ${d.name} (id: ${d.short})`).join("\n"));
-}
--- src/oneshot/remove.ts
+++
@@ -1,7 +0,0 @@
-import remove from "../funcs/remove";
-
-export default async function(...toRemove: string[]) {
-    for (const i of toRemove) {
-        remove(i);
-    }
-}
