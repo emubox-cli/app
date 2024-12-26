40627b45dfabce90e0e78c2e3e2632645db7180a
Skullbite <git@bork.quest>
2024-12-26 15:10:44 -0500

    check shas for update

--- src/cmd/update.ts
+++ src/cmd/update.ts
@@ -2,8 +2,14 @@ import { $ } from "bun";
 import containerPrefix from "utils/containerPrefix";
 
 export default async function() {
-    await $`
-        ${containerPrefix} paru -Syu
-        curl -o $HOME/.local/bin/emubox https://emubox.wolves-are.gay/emubox
-    `;
+    await $`${containerPrefix} paru -Syu`;
+    const latest = await $`curl https://emubox.wolves-are.gay/latest`.text();
+
+    // @ts-expect-error defined in build
+    if (_SHA !== latest) {
+        console.log("Updating emubox...");
+        await $`curl -o $HOME/.local/bin/emubox https://emubox.wolves-are.gay/emubox`.text();
+    } else 
+        console.log("Emubox is up to date");
+    
 }
