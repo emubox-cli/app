280b243962c2e564b71272f665155631c2d647df
skullbite <git@bork.quest>
2025-04-18 10:53:06 -0400

    update command redirects you now, sadge

--- src/cmd/update.ts
+++ src/cmd/update.ts
@@ -1,17 +1,15 @@
 import { $ } from "bun";
-import chalk from "chalk";
+import { ppid } from "process";
 import containerPrefix from "utils/containerPrefix";
 
 export default async function() {
-    await $`${containerPrefix} paru -Syu`;
+    const parentCmd = await $`cat /proc/${ppid}/comm`.text();
+    if (parentCmd !== "emubox-update") {
+        console.log("Please run 'emubox-update', apologies for the inconvience.");
+        return;
+    }
+    await $`${containerPrefix}paru -Syu --noconfirm`.nothrow();
+    // await $`${containerPrefix}flatpak -U`
     // todo: update appimages
-    const latest = (await $`curl https://emubox.wolves-are.gay/latest`.text()).replace("\n", "");
-
-    // @ts-expect-error defined in build
-    if (_SHA !== latest) {
-        console.log(chalk.bold("Update the emubox manager by running the following command:"));
-        console.log('sh -c "$(curl -sSL https://emubox.wolves-are.gay/install)" -u');
-    } else 
-        console.log("Emubox is up to date");
     
 }
