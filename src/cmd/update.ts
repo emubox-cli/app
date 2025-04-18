2126d2bf09796616ee7bce6f4b98ffbee99643af
skullbite <git@bork.quest>
2025-04-18 14:08:18 -0400

    nvm, i'm not doing anything in the internal update command now

--- src/cmd/update.ts
+++ src/cmd/update.ts
@@ -1,15 +1,10 @@
-import { $ } from "bun";
-import { ppid } from "process";
-import containerPrefix from "utils/containerPrefix";
-
 export default async function() {
+    console.log("Please run 'emubox-update', apologies for the inconvience.");
+    /*
     const parentCmd = await $`cat /proc/${ppid}/comm`.text();
     if (parentCmd !== "emubox-update") {
         console.log("Please run 'emubox-update', apologies for the inconvience.");
         return;
-    }
-    await $`${containerPrefix}paru -Syu --noconfirm`.nothrow();
-    // await $`${containerPrefix}flatpak -U`
-    // todo: update appimages
-    
+    } 
+    */
 }
