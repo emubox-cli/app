56eb3772a151e9c1de6090d7a14c1c7aeefe1dde
skullbite <git@bork.quest>
2025-09-06 09:15:34 -0400

    beefed the hell out of the list command

--- src/cmd/ls.ts
+++ src/cmd/ls.ts
@@ -1,5 +1,44 @@
+import parseFlags from "utils/parseFlags";
 import apps from "../utils/apps";
+import { openConfig } from "utils/config";
 
-export default function() {
-    console.log(apps.filter(d => d.installOptions.aur).map(d => `- ${d.name} (id: ${d.id})`).join("\n"));
+export default async function(...args: string[]) {
+    const { flags: { installed: isInstalled } } = parseFlags(args, {
+        "installed": ["-i", "--installed"],
+    });
+
+    
+    const config = await openConfig();
+    const installedApps = config.installed.map(d => d.id);
+    console.log(
+        apps.a.filter(
+            d => {
+                if (isInstalled) 
+                    return installedApps.includes(d.i);
+                return true;
+            }
+        )
+        .sort((a, b) => (installedApps.includes(a.i) === installedApps.includes(b.i)) ? 0 : installedApps.includes(a.i) ? 1 : -1)
+        .map(d => { 
+            let installationStatement = "";
+            if (installedApps.includes(d.i)) 
+                installationStatement = `Installed with: ${config.installed.find(dd => d.i == dd.id)?.source}`;
+            else {
+                const installOpts = [];
+                if (d.o.includes("a"))
+                    installOpts.push("AUR");
+                if (d.o.includes("f"))
+                    installOpts.push("FLATPAK");
+                if (d.o.includes("g"))
+                    installOpts.push("APPIMAGE");
+                if (d.o.includes("m"))
+                    installOpts.push("MANUAL_PROVIDE_REQUIRED");
+
+                installationStatement = `(${installOpts.join(", ")})`;
+            }
+            const line = `- ${d.i}: ${d.n}`;
+            return `${line}${" ".repeat(50 - line.length)}| ${installationStatement}`; 
+        })
+        .join("\n")
+    );
 }
