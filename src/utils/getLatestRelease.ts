748333e41d00dc5c1a5c0dfbb008bc63cb243e8a
skullbite <git@bork.quest>
2025-09-19 00:47:02 -0400

    releases are now properly sorted

--- src/utils/getLatestRelease.ts
+++ src/utils/getLatestRelease.ts
@@ -1,9 +1,7 @@
 export default async function(repo: string) {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const releases: { [x: string]: any }[] = await (await fetch(`https://api.github.com/repos/${repo}/releases`)).json() as any;
-    let latest = releases[0];
-    if (latest.prerelease) 
-        latest = releases[1];
-    
-    return latest;
+
+    // @ts-expect-error typical js amirite
+    return releases.sort((a, b) => new Date(b.published_at) - new Date(a.published_at))[0];
 }
