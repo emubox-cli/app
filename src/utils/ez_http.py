581b2e83d2bad10d9b7e1e108d3bb3e886f31125
astrid-axo <git2@bork.quest>
2026-01-15 00:21:32 -0500

    stop tracking emubox.spec

--- src/utils/ez_http.py
+++ src/utils/ez_http.py
@@ -1,14 +1,11 @@
-
-import aiohttp, aiofiles, certifi, ssl
+import aiohttp, aiofiles
 from os import chmod
 import stat
 
 # not tryna type aiohttp.ClientSession everywhere
 async def request(url: str, headers={}):
     print(f"REQUEST: {url}")
-    ssl_ctx = ssl.create_default_context(cafile=certifi.where())
-    conn = aiohttp.TCPConnector(ssl=ssl_ctx)
-    async with aiohttp.ClientSession(connector=conn) as sess:
+    async with aiohttp.ClientSession() as sess:
         async with sess.get(url, headers=headers) as req:
             return await req.json()
 
